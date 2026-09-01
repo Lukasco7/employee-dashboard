70619825cd72ee794632b00420467b3424ccfa1e
Lukman Mohammed <lukman728@gmail.com>
2026-09-01 19:30:59 +0000

    Finalize sales layout and digital calculator

--- components/Sales.tsx
+++ components/Sales.tsx
@@ -922,7 +922,7 @@ export default function Sales({
                       e.target.value
                     )
                   }
-                  className="w-full px-4 py-3 border border-gray-300 rounded-lg bg-white text-gray-900 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  className="w-full px-4 py-3 border border-gray-300 rounded-lg bg-white text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
                   required
                 >
 
